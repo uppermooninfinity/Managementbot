e1d058cbfb8eea3bbe0c815d7e043ec4ae7b20de
ARJUN <pachangedinanath@gmail.com>
2026-05-06 22:57:26 +0530

    𝐄𝗌ρ𝗋ⱺ 𝐌ᥙsiᥴ [ ɴᴏ ᴀᴅʂ ] 🕊

--- AloneRobot/__init__.py
+++ AloneRobot/__init__.py
@@ -160,7 +160,7 @@ telethn = TelegramClient("alone", API_ID, API_HASH)
 pbot = Client("AloneRobot", api_id=API_ID, api_hash=API_HASH, bot_token=TOKEN,in_memory=True)
 dispatcher = updater.dispatcher
 aiohttpsession = ClientSession()
-YouTube = YouTubeAPI()
+
 
 print("[INFO]: Getting Bot Info...")
 BOT_ID = dispatcher.bot.id
